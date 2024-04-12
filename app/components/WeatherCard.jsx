489261d8b4485d0ec08c889d2e768db23a04524c
Vlad Alvarez <moroni.v.alvarez@gmail.com>
2024-04-12 08:36:13 -0600

    set weather icon function

--- app/components/WeatherCard.jsx
+++ app/components/WeatherCard.jsx
@@ -14,13 +14,26 @@ export default function WeatherCard({
   const [icon, setIcon] = useState(sun)
   const { time } = useDate()
 
+  // set weather card icon
   useEffect(() => {
-    first
-  
-    return () => {
-      second
+    if(iconString) {
+      if(iconString.toLowerCase().includes('cloud')) {
+        setIcon(cloud)
+      } else if(iconString.toLowerCase().includes('rain')) {
+        setIcon(rain)
+      } else if(iconString.toLowerCase().includes('clear')) {
+        setIcon(sun)
+      } else if(iconString.toLowerCase().includes('thunder')) {
+        setIcon(storm)
+      } else if(iconString.toLowerCase().includes('fog')) {
+        setIcon(fog)
+      } else if(iconString.toLowerCase().includes('snow')) {
+        setIcon(snow)
+      } else if(iconString.toLowerCase().includes('wind')) {
+        setIcon(wind)
+      }
     }
-  }, [third])
+  }, [iconString])
   
 
   return (
