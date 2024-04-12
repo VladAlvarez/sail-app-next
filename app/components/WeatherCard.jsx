e08be9a7cf2e4736147cf02725062dec0bbbce36
Vlad Alvarez <moroni.v.alvarez@gmail.com>
2024-04-12 09:15:06 -0600

    weather card basic css

--- app/components/WeatherCard.jsx
+++ app/components/WeatherCard.jsx
@@ -1,6 +1,7 @@
 "use client"
 import { useEffect, useState } from "react"
 import { useDate } from '../Utils/useDate'
+import '../globals.css'
 
 export default function WeatherCard({
   temperature,
@@ -37,7 +38,26 @@ export default function WeatherCard({
   
 
   return (
-    <>
-    </>
+    <div className="w-[22rem] min-w-[22rem] h-[30rem] weatherCard p-4">
+      <div className="flex w-full justify-center, items-center gap-4 mt-12 mb-4">
+        <Img src={icon} alt="weather-icon" />
+        <p className="font-bold text-2xl flex justify-center items-center">{temperature} &deg;F</p>
+      </div>
+      <div className="font-bold text-center text-xl">
+        {place}
+      </div>
+      <div className="w-full flex justify-between items-center mt-4">
+        <p className="flex-1 text-center p-2">{new Date().toDateString()}</p>
+        <p className="flex-1 text-center p-2">{time}</p>
+        <div className="w-full flex justify-center items-center mt-4 gap-4">
+          <p className="flex-1 text-center p-2 font-bold bg-blue-600 shadow rounded-lg">Wind Speed <p className="font-normal">{windspeed}</p></p>
+          <p className="flex-1 text-center p-2 font-bold rounded-lg bg-green-600">Humidity <p className="font-normal">{humidity}</p></p>
+        </div>
+        <hr className="bg-slate-600"/>
+        <div className="w-full p-4 flex justify-center items-center text-xl font-semibold">
+          {conditions}
+        </div>
+      </div>
+    </div>
   )
 }
