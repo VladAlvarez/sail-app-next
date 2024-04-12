aab4e56ff6279eb45906b5c27ae2c834710ea3f9
Vlad Alvarez <moroni.v.alvarez@gmail.com>
2024-04-12 08:18:47 -0600

    first vlad/weather commit

--- app/components/WeatherCard.jsx
+++ app/components/WeatherCard.jsx
@@ -1,5 +1,30 @@
-export default function WeatherCard() {
+"use client"
+import { useEffect, useState } from "react"
+import { useDate } from '../Utils/useDate'
+
+export default function WeatherCard({
+  temperature,
+  windspeed,
+  humidity,
+  place,
+  iconString,
+  conditions
+}) {
+
+  const [icon, setIcon] = useState(sun)
+  const { time } = useDate()
+
+  useEffect(() => {
+    first
+  
+    return () => {
+      second
+    }
+  }, [third])
+  
+
   return (
-    <div></div>
+    <>
+    </>
   )
 }
