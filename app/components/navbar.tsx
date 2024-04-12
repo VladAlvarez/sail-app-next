10395c4d8eb1d79721cfcda43a106a59d6372102
Kye Clevenger <KyeClevenger@gmail.com>
2024-04-12 13:02:39 -0600

    kyeupdated

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,23 +1,28 @@
-import Link from 'next/link'
-import Image  from "next/image";
-import Logo from './sail-logo.png'
+import Link from 'next/link';
+import Image from 'next/image';
+import Logo from './sail-logo.png';
 
 export default function Navbar() {
   return (
-    <nav>
-      <Image
-        src={Logo}
-        alt='Sail logo'
-        width={50}
-        quality={100}
-        placeholder='blur' 
-      />
-    <Link href="/">Home</Link>
-    <Link href="/schedule">Schedule</Link>
-    <Link href="/contact">Contact</Link>
-    <Link href="/captains-log">Captain&apos;s Log</Link>
-    <Link href="/client-list">Client List</Link>
-    <Link href="/login">Login</Link>
-  </nav>
-  )
+    <nav className="flex items-center justify-between p-3 flex-wrap">
+      <div className="flex items-center">
+        <Image
+        className='logo'
+          src={Logo}
+          alt='Sail logo'
+          width={50}
+          quality={100}
+          placeholder='blur' 
+        />
+        <Link className="links" href="/">Home</Link>
+        <Link className="links" href="/schedule">Schedule</Link>
+        <Link className="links" href="/contact">Contact</Link>
+        <Link className="links" href="/captains-log">Captain&apos;s Log</Link>
+        <Link className="links" href="/client-list">Client List</Link>
+      </div>
+      <div>
+        <Link className="links" href="/login">Login</Link>
+      </div>
+    </nav>
+  );
 }
