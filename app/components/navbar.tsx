80945dc0da0889c7137fc5f682e701c6f51d91ea
Kye Clevenger <KyeClevenger@gmail.com>
2024-04-18 08:26:08 -0600

    login

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,10 +1,24 @@
+"use client"
+
 import Link from 'next/link';
 import Image from 'next/image';
 import Logo from './sail-logo.png';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/authContext';
+import { doSignOut } from '../firebase/auth';
 
 export default function Navbar() {
+  const navigate = useNavigate();
+  const { userLoggedIn } = useAuth();
+
   return (
     <nav className="flex items-center justify-between p-3 flex-wrap">
+      {
+        userLoggedIn ?
+        <button onClick={()=> {doSignOut().then(()=> {navigate('/login');})}}></button>
+        :
+        null
+      }
       <div className="flex items-center">
         <Image
           className='logo'
@@ -13,7 +27,7 @@ export default function Navbar() {
           width={50}
           quality={100}
           placeholder='blur'
-        />
+          />
         <Link className="links" href="/">Home</Link>
         <Link className="links" href="/schedule">Schedule</Link>
         <Link className="links" href="/contact">Contact</Link>
