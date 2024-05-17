5a1384744924f72319b3d28c8899e46435fa3f78
Vlad Alvarez <moroni.v.alvarez@gmail.com>
2024-05-17 11:07:17 -0600

    ran build

--- app/Context/index.tsx
+++ app/Context/index.tsx
@@ -1,8 +1,6 @@
 "use client"
-import { useContext, createContext, useState, useEffect } from "react";
+import { useContext, createContext, useState, useEffect, Dispatch, SetStateAction } from "react";
 import axios from 'axios';
-
-const StateContext = createContext('light');
 
 export interface WeatherItemType {
     dt_txt: string;
@@ -12,12 +10,26 @@ export interface WeatherItemType {
     windspeed: number;
     humidity: number;
     temperature: number;
+    setPlace: (str: string) => void;
+    values: number[];
+    thisLocation: string;
+    place: string;
   };
 
+
+const StateContext = createContext<{
+    weather: WeatherItemType | null,
+    setPlace: Dispatch<SetStateAction<string>>,
+    values: number[],
+    thisLocation: string,
+    place: string
+} | null>(null);
+
+
 export const StateContextProvider = ({ children }:any) => {
-    const [weather, setWeather] = useState<WeatherItemType[]>([]);
-    const [values, setValues] = useState([]);
-    const [place, setPlace] = useState('Lindon');
+    const [weather, setWeather] = useState<WeatherItemType | null>(null);
+    const [values, setValues] = useState<number[]>([]);
+    const [place, setPlace] = useState<string>('Lindon');
     const [thisLocation, setLocation] = useState('');
 
 // Fetch API
@@ -35,7 +47,7 @@ const fetchWeather = async () => {
         
         if (response && response.data && response.data.city && response.data.list) {
             const fiveDaysAtMidnight = response.data.list.filter(
-                currentTimeForecast => currentTimeForecast.dt_txt.includes('00:00:00')
+                (currentTimeForecast: { dt_txt: string }) => !!currentTimeForecast.dt_txt.includes('00:00:00')
             )
             // Update state with the location and forecast data
             setLocation(response.data.city.name);
@@ -59,7 +71,7 @@ const fetchWeather = async () => {
     }, [place]);
 
     return (
-        <StateContext.Provider value={{
+        <StateContext.Provider value={{    
             weather,
             setPlace,
             values,
